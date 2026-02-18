70d7b97f47b3d94be693ec8467a5de1a39f0ac95
gm4710-jpg <gm4710@srmist.edu.in>
2026-02-17 23:36:05 -0500

    Implement UC2: Multi-line ASCII banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,22 @@
-public class OOPSBannerApp
-{
-
+/**
+ * OOPSBannerApp UC2 Banner Display Application
+ * Renders "OOPS" using a 7-line ASCII grid.
+ */
+public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(" **** " + " " + " **** " + " " + "******* " + " " + " ***** ");
+        // Line 2
+        System.out.println("** **" + " " + "** **" + " " + "** **" + " " + "** **");
+        // Line 3
+        System.out.println("** **" + " " + "** **" + " " + "** **" + " " + "** ");
+        // Line 4
+        System.out.println("** **" + " " + "** **" + " " + "******* " + " " + " ***** ");
+        // Line 5
+        System.out.println("** **" + " " + "** **" + " " + "** " + " " + "     **");
+        // Line 6
+        System.out.println("** **" + " " + "** **" + " " + "** " + " " + "** **");
+        // Line 7
+        System.out.println(" **** " + " " + " **** " + " " + "** " + " " + " ***** ");
     }
 }
