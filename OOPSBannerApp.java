6e661cc92d3d38247777e239c2109cc7db7e617b
gm4710-jpg <gm4710@srmist.edu.in>
2026-02-19 11:14:35 -0500

    Added UC2 - Print OOPS Banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,14 @@
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object 
- * Oriented Programming System OOPS acronym to the console.
- *
- * @author Developer
- * @version 1.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Each line is 7 rows high and letters are formed using * and spaces
+        System.out.println("  *****     *****     ******     *****  ");
+        System.out.println(" *     *   *     *    *     *   *     * ");
+        System.out.println("*       * *       *   *     *   *       ");
+        System.out.println("*       * *       *   ******     *****  ");
+        System.out.println("*       * *       *   *               * ");
+        System.out.println(" *     *   *     *    *        *     *  ");
+        System.out.println("  *****     *****     *         *****   ");
+
     }
 }
